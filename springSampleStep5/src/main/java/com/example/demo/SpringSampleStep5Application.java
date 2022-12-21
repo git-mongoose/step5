961f824df3a865f451dc56fcb2d08980ba7b94ba
hama <hama@192.168.100.115>
2022-12-21 19:53:44 +0900

    springロードマップstep5を追加

---
+++ springSampleStep5/src/main/java/com/example/demo/SpringSampleStep5Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSampleStep5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSampleStep5Application.class, args);
+	}
+
+}
